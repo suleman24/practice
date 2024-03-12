33974938ac50cc3cb0b1e5a3cf4dac9a041bede9
Suleman Anwar <63181973+suleman24@users.noreply.github.com>
2024-03-12 15:31:18 +0500

    @includeWhen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,3 +13,12 @@
 
 @include('pages.footer',['arr' => $arr])
 
+@includeWhen(true,'pages.header',['name' => 'True'])
+@includeWhen(false,'pages.header',['name' => 'False'])
+@includeWhen(5==2,'pages.header',['name' => '5==2'])
+@includeWhen(5==5,'pages.header',['name' => '5==5'])
+
+
+
+
+
